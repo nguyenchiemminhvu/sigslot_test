d1e5cccc495c87c296a9dff7f00decee07500732
vu.nguyenchiemminh <vu.nguyenchiemminh@gameloft.com>
2019-03-12 15:00:16 +0700

    adsf

--- SigSlotTest/main.cpp
+++ SigSlotTest/main.cpp
@@ -1,36 +1,37 @@
 #include "sigslot\sigslot.h"
 #include <iostream>
 
-using namespace sigslot;
+#define EXAMPLE_SWITCH_AND_LIGHT_BASIC			1
+#define EXAMPLE_SWITCH_AND_LIGHT_NEW			2
+#define EXAMPLE_CONNECTING_SIGNALS_WITH_SLOTS	3
 
-class Window
-{
-public:
-	Window() { }
-	void SetSize(int width, int height)
-	{
-		Resized(width, height);
-	}
+#define EXAMPLE									1
 
-public: // signals
-	signal2<int, int> Resized;
-};
-
-class WindowMoniter : public has_slots<>
-{
-public: // slots
-	void OnResize(int width, int height)
-	{
-		std::cout << "Window resized to " << width << " x " << height << std::endl;
-	}
-};
+#if EXAMPLE == EXAMPLE_SWITCH_AND_LIGHT_BASIC
 
 int main(int argc, char **argv)
 {
-	Window w;
-	WindowMoniter m;
-	w.Resized.connect(&m, &WindowMoniter::OnResize);
-	w.SetSize(1024, 768);
+	
 
 	return 0;
 }
+
+#elif EXAMPLE == EXAMPLE_SWITCH_AND_LIGHT_NEW
+
+int main(int argc, char **argv)
+{
+
+
+	return 0;
+}
+
+#elif EXAMPLE == EXAMPLE_CONNECTING_SIGNALS_WITH_SLOTS
+
+int main(int argc, char **argv)
+{
+
+
+	return 0;
+}
+
+#endif
